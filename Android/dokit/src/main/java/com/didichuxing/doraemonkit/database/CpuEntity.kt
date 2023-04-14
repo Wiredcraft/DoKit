a9a3931093ac4467b80cd2901a52be3c6f1ec989
shawn <zhang.shawn@externals.adidas.com>
2023-04-14 14:17:30 +0800

    feat: support database migration

--- Android/dokit/src/main/java/com/didichuxing/doraemonkit/database/CpuEntity.kt
+++ Android/dokit/src/main/java/com/didichuxing/doraemonkit/database/CpuEntity.kt
@@ -1,10 +1,12 @@
 package com.didichuxing.doraemonkit.database
 
+import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
 @Entity
 data class CpuEntity(
     @PrimaryKey val time: Long,
+    @ColumnInfo(defaultValue = "0")
     val usageRate: Long,
 )
