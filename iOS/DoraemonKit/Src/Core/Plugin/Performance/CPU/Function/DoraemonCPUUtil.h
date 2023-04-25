748b6769050afafa3fe7c21c7d1c22efbe2e3c3b
Jun Ma (C) <jun.ma@wiredcraft.com>
2023-04-25 17:02:26 +0800

    MOB-408: [iOS SDK] Get CPU usage and call stack for specific thread

--- iOS/DoraemonKit/Src/Core/Plugin/Performance/CPU/Function/DoraemonCPUUtil.h
+++ iOS/DoraemonKit/Src/Core/Plugin/Performance/CPU/Function/DoraemonCPUUtil.h
@@ -7,10 +7,12 @@
 
 #import <Foundation/Foundation.h>
 #import <UIKit/UIKit.h>
+#import "DoraemonCPUUsageCallStackModel.h"
 
 @interface DoraemonCPUUtil : NSObject
 
 //获取CPU使用率
 + (CGFloat)cpuUsageForApp;
++ (nullable DoraemonCPUUsageCallStackModel *)cpuUsageAndCallstackForThread;
 
 @end
