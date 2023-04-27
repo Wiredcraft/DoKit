13e12d5032ae561615dbdbfd9da7ae616ac161dc
shawn <zhang.shawn@externals.adidas.com>
2023-04-27 14:29:24 +0800

    feat: MOB-437 The ANR's data seems not correct

--- Android/dokit/src/main/java/com/didichuxing/doraemonkit/kit/webdoor/bean/BlockBean.kt
+++ Android/dokit/src/main/java/com/didichuxing/doraemonkit/kit/webdoor/bean/BlockBean.kt
@@ -5,10 +5,12 @@ import com.didichuxing.doraemonkit.database.BlockEntity
 data class BlockBean(
     val duration: Long,
     val info: String,
+    val count: Int,
 )
 
 fun convertToBlockFrom(cpuEntities: List<BlockEntity>): List<BlockBean> {
-    return cpuEntities.map {
-        BlockBean(it.timeCost, it.info)
-    }
+    return cpuEntities.filter { !isDoKitClass(it.info) }.groupBy { it.info }.entries.map {
+        val avgDuration = it.value.map { it.timeCost }.average()
+        BlockBean(avgDuration.toLong(), it.key, it.value.size)
+    }.sortedByDescending { it.duration }.take(5)
 }
